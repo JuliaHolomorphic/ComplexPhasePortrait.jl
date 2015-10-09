a41727a19f7485025dec1ed9873ae986d6eabbbb
Everett Kropf <ehkropf@gmail.com>
2015-10-09 11:16:13 +0100

    Add property setting to Image constructor call.

--- src/ComplexPhasePortrait.jl
+++ src/ComplexPhasePortrait.jl
@@ -2,7 +2,8 @@ module ComplexPhasePortrait
 
 using Images, Colors
 
-export portrait, PortraitType, PTproper, PTcgrid, PTstepphase, PTstepmod
+export portrait,
+       PortraitType, PTproper, PTcgrid, PTstepphase, PTstepmod
 
 @enum PortraitType PTproper PTcgrid PTstepphase PTstepmod
 
@@ -20,14 +21,18 @@ The PortraitType determines what type of portrait is generated:
 
 Named arguments:
 
-* ctype - is the colormap type. The default "standard" is a basic HSL map with H∈[0,360], S = 1.0, and L = 0.5. Setting `ctype="nist"` gives the [NIST color scheme](http://dlmf.nist.gov/help/vrml/aboutcolor).
-* pres - is the phase step resolution (number of phase jumps), default `pres=20`.
+* ctype - is the colormap type. The default "standard" is a basic HSL map
+  with H∈[0,360], S = 1.0, and L = 0.5. Setting `ctype="nist"` gives the
+  [NIST color scheme](http://dlmf.nist.gov/help/vrml/aboutcolor).
+* pres - is the phase step resolution (number of phase jumps), default
+  `pres=20`.
 """
 function portrait(fval::Array{Complex{Float64},2}, mtype::PortraitType=PTproper;
                   ctype="standard", pres=20)
     (farg, nphase, cm) = setupPhase(fval, ctype)
     (n, m) = size(nphase)
-    img = Image(Array(RGB{Float64}, n, m))
+    img = Image(Array(RGB{Float64}, n, m), spatialorder=["y", "x"],
+                colorspace="RGB", colordim=0)
 
     if mtype == PTproper
       phaseToImage!(img, nphase, cm)
@@ -58,7 +63,7 @@ end
 "Color map for complex phase portrait with 600 elements."
 function baseColorMap(ctype="standard")
   if ctype == "nist"
-        nc = 900
+        const nc = 900
         cm = linspace(HSL(0.0, 1.0, 0.5), HSL(360.0, 1.0, 0.5), nc)
         idx = [1:Int(nc/6); Int(nc/6)+1:2:Int(nc/2); Int(nc/2)+1:2*Int(nc/3);
               2*Int(nc/3)+1:2:nc]
