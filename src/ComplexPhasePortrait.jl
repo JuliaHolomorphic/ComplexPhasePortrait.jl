69ba03b0077c3258c76ba749a09ec5a20d86ec8e
Sheehan Olver <solver@mac.com>
2017-10-09 09:53:15 +0100

    Fixes for 0.6

--- src/ComplexPhasePortrait.jl
+++ src/ComplexPhasePortrait.jl
@@ -1,16 +1,15 @@
 module ComplexPhasePortrait
-
-import Images.Image
+import Images
 import Colors: RGB, HSL
 
 export portrait,
        PTproper, PTcgrid, PTstepphase, PTstepmod
 
-abstract PortraitType <: Any
-type PTproper <: PortraitType end
-type PTcgrid <: PortraitType end
-type PTstepmod <: PortraitType end
-type PTstepphase <: PortraitType end
+abstract type PortraitType <: Any end
+struct PTproper <: PortraitType end
+struct PTcgrid <: PortraitType end
+struct PTstepmod <: PortraitType end
+struct PTstepphase <: PortraitType end
 
 const brighten = 0.1
 
@@ -57,7 +56,7 @@ end
 function portrait(fval::Array{Complex{Float64},2}, ::Type{PTstepmod};
                   pres=20, kwargs...)
     (img, nphase, cm) = baseArgs(fval; kwargs...)
-    phaseToImage!(img, nphase, sawfun(log(abs(fval)), 2pi/pres, 0.75, 1.0), cm)
+    phaseToImage!(img, nphase, sawfun(log.(abs.(fval)), 2pi/pres, 0.75, 1.0), cm)
     return img
 end
 
@@ -71,14 +70,14 @@ end
 function baseArgs(fval::Array{Complex{Float64},2}; kwargs...)
     (farg, nphase, cm) = setupPhase(fval; kwargs...)
     (n, m) = size(nphase)
-    img = Array(RGB{Float64}, m, n)
+    img = Array{RGB{Float64}}(m, n)
     return img, nphase, cm, farg
 end
 
 function setupPhase(fval; kwargs...)
     cm = baseColorMap(;kwargs...)
     nc = length(cm)
-    farg = (angle(-fval) + pi)/2pi
+    farg = (angle.(-fval) .+ pi)./2pi
     nphase = stepfun(farg, nc)
 
     return (farg, nphase, cm)
@@ -100,27 +99,29 @@ end
 
 "Integer step function with period 1 such that [0,1]⟶[1,nmax]."
 function stepfun(x, nmax)
-    y = x - floor(x)
-    y = convert(Array{UInt16}, floor(nmax*y) + 1)
+    y = x .- floor.(x)
+    y = convert(Array{UInt16}, floor.(nmax*y) .+ 1)
 end
 
 "Sawtooth function over reals with period dx onto [a,b]."
 function sawfun(x, dx, a, b)
     x = x/dx
-    x = x - floor(x)
-    x = a + (b - a)*x
+    x = x .- floor.(x)
+    x = a .+ (b - a)*x
 end
 
 function phaseToImage!(img, pidx, cm)
-    for j in 1:size(pidx, 2), i in 1:size(pidx, 1)
-        img[i,j] = cm[pidx[i,j]]
+    n = size(pidx, 1)
+    for j in 1:size(pidx, 2), i in 1:n
+        img[n-i+1,j] = cm[pidx[i,j]]
     end
     nothing
 end
 
 function phaseToImage!(img, pidx, black, cm)
-    for j in 1:size(pidx, 2), i in 1:size(pidx, 1)
-        img[i,j] = cm[pidx[i,j]]*black[i,j]
+    n = size(pidx, 1)
+    for j in 1:size(pidx, 2), i in 1:n
+        img[n-i+1,j] = cm[pidx[i,j]]*black[i,j]
     end
     nothing
 end
